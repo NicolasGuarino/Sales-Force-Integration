0ea63854026e2948cdff2e48ea5aaa1baded3e1a
root <tito_roosch@hotmail.com>
2019-09-12 15:04:37 -0300

    customized authentication

--- src/Services/Auth/Methods/GetToken.php
+++ src/Services/Auth/Methods/GetToken.php
@@ -4,47 +4,54 @@ namespace devnick\salesforce\Services\Auth\Methods;
 
 use GuzzleHttp\Client;
 
-class GetToken
+class GetToken{   
 
-{   
-
-    public function execute($account_id, $scope){
-        try
-        {    
+    public function execute($account_id, $scope, $auth_data = null)
+    {
+        try{    
             $headers = [
                 'Accept'        => 'application/json',
                 'Content-Type'  => 'application/json',
             ];
     
-            $body = [
-                'grant_type'    => 'client_credentials',
-                'client_id'     => env('CLIENT_ID'),
-                'client_secret' => env('CLIENT_SECRET'),
-                'scope'         => $scope,
-                'account_id'    => $account_id,
-            ];
-    
-    
             $client = new Client(
                 ['headers' => $headers]
             );
 
-            
-            $response = $client->request('POST', config('salesforce.url.auth.v2'), [
-                'body' => json_encode($body)
-            ]);
-            
+            if($auth_data){
+                $base_uri = $auth_data->base_uri;
+                $body = [
+                    'grant_type'    => 'client_credentials',
+                    'client_id'     => $auth_data->client_id,
+                    'client_secret' => $auth_data->client_secret,
+                    'scope'         => $scope,
+                    'account_id'    => $account_id,
+                ];
+
+                $response = $client->request('POST', $base_uri, [
+                    'body' => json_encode($body)
+                ]);
+            }else{
+                $body = [
+                    'grant_type'    => 'client_credentials',
+                    'client_id'     => env('CLIENT_ID'),
+                    'client_secret' => env('CLIENT_SECRET'),
+                    'scope'         => $scope,
+                    'account_id'    => $account_id,
+                ];
+
+                $response = $client->request('POST', config('salesforce.url.auth.v2'), [
+                    'body' => json_encode($body)
+                ]);
+            }
             $auth = json_decode($response->getBody()->getContents(), true);
 
             return (object) $auth;
-
         }catch(\Exception $error){
             return [
                 "status"    => $error->getCode(), 
                 "msg"       => $error->getMessage()
             ];
         }
-        
-
     }
 }
